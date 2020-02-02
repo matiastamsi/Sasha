bae3811e9b0027b092f73c0e99202a63b9f06be7
Santtu Lempinen <santtu.lempinen@helsinki.fi>
2020-02-02 14:29:04 +0200

    initial commit

---
+++ src/main/java/tkoaly/pohinatiimi/laudekoodit/LaudeBot.java
@@ -0,0 +1,44 @@
+package tkoaly.pohinatiimi.laudekoodit;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class LaudeBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        
+        Long chatId = update.getMessage().getChatId();
+        String text = update.getMessage().getText();
+        
+        // Initialize answer message
+        SendMessage answer = new SendMessage()
+            .setChatId(chatId);
+        
+        // Choose text for answer
+        if (text.equals("Hello")) {
+            answer.setText("World!");
+        } else {
+            answer.setText("Happy hacking!");
+        }
+        
+        // Send message
+        try {
+            execute(answer);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "INSERT BOT USERNAME HERE";
+    }
+
+    @Override
+    public String getBotToken() {
+        return System.getenv("INSERT ENVIRONMENT VARIABLE WITH TOKEN HERE");
+    }
+}
