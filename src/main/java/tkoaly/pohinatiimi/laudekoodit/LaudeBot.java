ea40b25e42d815df5c528227d02cc46d3fe2925e
Matias Tamsi <matias.tamsi@gmail.com>
2020-03-13 19:40:23 +0200

    korjaus

--- src/main/java/tkoaly/pohinatiimi/laudekoodit/LaudeBot.java
+++ src/main/java/tkoaly/pohinatiimi/laudekoodit/LaudeBot.java
@@ -5,6 +5,8 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import io.github.cdimascio.dotenv.Dotenv;
+import java.util.*;
+import java.io.*;
 
 public class LaudeBot extends TelegramLongPollingBot {
 
@@ -12,21 +14,29 @@ public class LaudeBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        
+
         Long chatId = update.getMessage().getChatId();
         String text = update.getMessage().getText();
-        
+
         // Initialize answer message
         SendMessage answer = new SendMessage()
-            .setChatId(chatId);
-        
-        // Choose text for answer
-        if (text.equals("Hello")) {
-            answer.setText("World!");
-        } else {
-            answer.setText("Happy hacking!");
+                .setChatId(chatId);
+
+        Random r = new Random();
+        try (Scanner tiedostonLukija = new Scanner(new File("testitiedosto.txt"))) {
+            int j = 1 + r.nextInt(500);
+            // luetaan tiedostoja kunnes kaikki rivit on luettu
+           
+            for (int i = 0; i < j; i++) {
+                tiedostonLukija.nextLine();
+            }
+            
+            System.out.println(tiedostonLukija.nextLine());
+            
+        } catch (Exception e) {
+            System.out.println("Virhe: " + e.getMessage());
         }
-        
+
         // Send message
         try {
             execute(answer);
